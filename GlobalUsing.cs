6ca77c6529d2188271176645367f12dd675998c8
mennaelnemr99 <mennaelnemr8@gmail.com>
2021-11-30 22:57:38 +0200

    Reformatted code

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,5 +1,5 @@
-global using System.Text.Json;
 global using System;
+global using System.Text.Json;
 global using System.IO;
 global using Spectre.Console;
 global using System.Collections.Generic;
